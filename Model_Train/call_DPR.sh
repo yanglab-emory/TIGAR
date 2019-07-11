2a67ed049c18dd6c390417f7ec6c9493bea40973
xmeng34 <43245050+xmeng34@users.noreply.github.com>
2019-07-11 19:57:46 -0400

    Update call_DPR.sh

--- Model_Train/call_DPR.sh
+++ Model_Train/call_DPR.sh
@@ -5,8 +5,6 @@ dpr_num=$2
 TargetID=$3
 out_prefix=$4
 
-DPR=/mnt/icebreaker/data2/home/snagpal/DPR/DPR-master/DPR
-
 BIMBAM=${file_dir}/bimbam
 PHENO=${file_dir}/pheno
 SNP=${file_dir}/SNP_annot
@@ -14,7 +12,7 @@ SNP=${file_dir}/SNP_annot
 cd ${out_prefix}
 
 echo $TargetID
-${DPR} \
+DPR \
 -g ${BIMBAM}/${TargetID}_bimbam.txt \
 -p ${PHENO}/${TargetID}_pheno.txt \
 -a ${SNP}/${TargetID}_snp_annot.txt \
