0961e97989ea80556e5538e2c3485a639c1795ec
Randy Parrish <rparrish@node08.local>
2020-06-16 19:55:53 -0400

    fixed issue with missing/non-overlapping SNPs btwn the zscore, weight, cov files

--- TWAS/Asso_Study_02.py
+++ TWAS/Asso_Study_02.py
@@ -99,90 +99,108 @@ def thread_process(num):
                                       stdout=subprocess.PIPE,
                                       stderr=subprocess.PIPE)
     Weight_out = Weight_process.communicate()[0]
-    
-    if len(Zscore_out)==0 | len(Weight_out) == 0:
+
+    if (len(Zscore_out)==0) | (len(Weight_out) == 0):
         print("There is no test SNP with non-zero cis-eQTL weights for gene:"+TargetID[num])
-    else:
-        print("TWAS for gene:"+TargetID[num])
-        Zscore = pd.read_csv(StringIO(Zscore_out.decode('utf-8')),sep='\t',header=None,low_memory=False)
-        Zscore.columns = np.array(tuple(Zscore_names))
+        return None
 
-        Weight = pd.read_csv(StringIO(Weight_out.decode('utf-8')),sep='\t',header=None,low_memory=False)
-        Weight.columns = np.array(tuple(Weight_names))
+    print("TWAS for gene:"+TargetID[num])
+    Zscore = pd.read_csv(StringIO(Zscore_out.decode('utf-8')),sep='\t',header=None,low_memory=False)
+    Zscore.columns = np.array(tuple(Zscore_names))
 
+    Weight = pd.read_csv(StringIO(Weight_out.decode('utf-8')),sep='\t',header=None,low_memory=False)
+    Weight.columns = np.array(tuple(Weight_names))
+
+    
+    Zscore['snpID'] = (Zscore['CHROM'].astype('str')+':'+Zscore['POS'].astype('str')
+                       +':'+Zscore.REF+':'+Zscore.ALT)
+
+    Weight['snpID'] = (Weight['CHROM'].astype('str')+':'+Weight['POS'].astype('str')
+                       +':'+Weight.REF+':'+Weight.ALT)
+
+    snp_overlap = np.intersect1d(np.array(Weight.snpID),np.array(Zscore.snpID))
+
+    if snp_overlap.size == 0:
+        print("There are no test SNPs with both non-zero cis-eQTL weights and GWAS summary statistics for gene:"+TargetID[num])
+        return None
+
+    # merge Zscore and Weight File
+    ZW = (Weight >> mask(Weight.snpID.isin(snp_overlap))).merge((Zscore
+                                                                 >>mask(Zscore.snpID.isin(snp_overlap))
+                                                                 >>select(Zscore.snpID,Zscore.Zscore)),
+                                                                left_on='snpID',
+                                                                right_on='snpID')
+    ZW = ZW.drop_duplicates(['snpID'],keep='first')
+    ZW = ZW.sort_values(by='POS')
+    ZW = ZW.reset_index(drop=True)
+    
+    ### Read in reference covariance matrix file
+    covar_process = subprocess.Popen(["tabix"+" "+args.LD+" "+str(args.chr)+":"+str(min(ZW.POS))+"-"+str(max(ZW.POS))],
+                                     shell=True,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+    
+    out=covar_process.communicate()[0]
+    if len(out)==0:
+        print("No reference covariance information for Gene:"+TargetID[num])
+        return None
+    
+    MCOV = pd.read_csv(StringIO(out.decode('utf-8')),sep='\t',header=None,low_memory=False)
+    MCOV.columns = ['CHROM','POS','ID','REF','ALT','COV']
+    
+    MCOV['snpID'] = (MCOV['CHROM'].astype('str')+':'+MCOV['POS'].astype('str')
+                     +':'+MCOV.REF+':'+MCOV.ALT)
+    
+    snp_overlap_cov = np.intersect1d(snp_overlap,np.array(MCOV.snpID))
+    
+    if snp_overlap_cov.size == 0:
+         print("No reference covariance information for target SNPs for gene:"+TargetID[num])
+         return None
+
+    MCOV = MCOV.drop_duplicates(['snpID'],keep='first')
+
+    MCOV['COV'] = MCOV['COV'].apply(lambda x:np.array(x.split(',')).astype('float'))
+    MCOV = MCOV.sort_values(by='POS')
+    MCOV = MCOV.reset_index(drop=True)
+    
+    indicates = (MCOV >> mask(MCOV.snpID.isin(ZW.snpID))).index
+    
+    V_temp = np.zeros((len(indicates),len(indicates)))
+    
+    for i in range(len(indicates)):
+        cov_temp = MCOV.COV.loc[indicates[i]]
+        N = len(cov_temp)
         
-        Zscore['snpID'] = (Zscore['CHROM'].astype('str')+':'+Zscore['POS'].astype('str')
-                           +':'+Zscore.REF+':'+Zscore.ALT)
-
-        Weight['snpID'] = (Weight['CHROM'].astype('str')+':'+Weight['POS'].astype('str')
-                           +':'+Weight.REF+':'+Weight.ALT)
-
-        snp_overlap = np.intersect1d(np.array(Weight.snpID),np.array(Zscore.snpID))
-        # merge Zscore and Weight File
-        ZW = (Weight >> mask(Weight.snpID.isin(snp_overlap))).merge((Zscore
-                                                                     >>mask(Zscore.snpID.isin(snp_overlap))
-                                                                     >>select(Zscore.snpID,Zscore.Zscore)),
-                                                                    left_on='snpID',
-                                                                    right_on='snpID')
-        ZW = ZW.drop_duplicates(['snpID'],keep='first')
-        ZW = ZW.sort_values(by='POS')
-        ZW = ZW.reset_index(drop=True)
-
-        ### Read in reference covariance matrix file
-        covar_process = subprocess.Popen(["tabix"+" "+args.LD+" "+str(args.chr)+":"+str(min(ZW.POS))+"-"+str(max(ZW.POS))],
-                                         shell=True,
-                                         stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE)
-        
-        out=covar_process.communicate()[0]
-        if len(out)==0:
-            print("No reference covariance information for Gene:"+TargetID[num])
-        
-        else:
-            MCOV = pd.read_csv(StringIO(out.decode('utf-8')),sep='\t',header=None,low_memory=False)
-            MCOV.columns = ['CHROM','POS','ID','REF','ALT','COV']
-            
-            MCOV['snpID'] = (MCOV['CHROM'].astype('str')+':'+MCOV['POS'].astype('str')
-                             +':'+MCOV.REF+':'+MCOV.ALT)
-            
-            MCOV = MCOV.drop_duplicates(['snpID'],keep='first')
-
-            MCOV['COV'] = MCOV['COV'].apply(lambda x:np.array(x.split(',')).astype('float'))
-            MCOV = MCOV.sort_values(by='POS')
-            MCOV = MCOV.reset_index(drop=True)
-            
-            indicates = (MCOV >> mask(MCOV.snpID.isin(ZW.snpID))).index
-            
-            V_temp = np.zeros((len(indicates),len(indicates)))
-            
-            for i in range(len(indicates)):
-                cov_temp = MCOV.COV.loc[indicates[i]]
-                N = len(cov_temp)
+        for j in range(i,len(indicates)):
+            if indicates[j] - indicates[i] < N:
+                V_temp[i,j] = cov_temp[indicates[j]-indicates[i]]
+            else:
+                V_temp[i,j] = 0
                 
-                for j in range(i,len(indicates)):
-                    if indicates[j] - indicates[i] < N:
-                        V_temp[i,j] = cov_temp[indicates[j]-indicates[i]]
-                    else:
-                        V_temp[i,j] = 0
-                        
-            V=V_temp+V_temp.T-np.diag(V_temp.diagonal())
+    V=V_temp+V_temp.T-np.diag(V_temp.diagonal())
+    
+    ZW = ZW >> mask(ZW.snpID.isin(MCOV.snpID))
+    ### Calculate burden Z score
+    burden_Z = mat(ZW.Zscore)*mat(ZW.ES).T/sqrt(mat(ZW.ES)*V*mat(ZW.ES).T)
+    
+    if np.isnan(burden_Z):
+    	print("Could not calculate burden_Z: NaN value.")
+    	return None
+    
+    result = pd.DataFrame()
+    result['CHROM'] = np.array(args.chr).ravel()
+    result['GeneStart'] = np.array(Gene_temp.GeneStart).ravel()
+    result['GeneEnd'] = np.array(Gene_temp.GeneEnd).ravel()
+    result['TargetID'] = np.array(TargetID[num]).ravel()
+    result['GeneName'] = np.array(Gene_temp.GeneName).ravel()
+    result['TWAS_Zscore'] = burden_Z
+    ### p-value for chi-square test
+    result['PVALUE'] = 1-chi2.cdf(burden_Z**2,1)
 
-            ZW = ZW >> mask(ZW.snpID.isin(MCOV.snpID))
-            ### Calculate burden Z score
-            burden_Z = mat(ZW.Zscore)*mat(ZW.ES).T/sqrt(mat(ZW.ES)*V*mat(ZW.ES).T)
+    result.to_csv(args.out_dir+'/CHR'+str(args.chr)+'_sumstat_assoc.txt',
+                  sep='\t',index=None,header=None,mode='a')
 
-            result = pd.DataFrame()
-            result['CHROM'] = np.array(args.chr).ravel()
-            result['GeneStart'] = np.array(Gene_temp.GeneStart).ravel()
-            result['GeneEnd'] = np.array(Gene_temp.GeneEnd).ravel()
-            result['TargetID'] = np.array(TargetID[num]).ravel()
-            result['GeneName'] = np.array(Gene_temp.GeneName).ravel()
-            result['TWAS_Zscore'] = burden_Z
-            ### p-value for chi-square test
-            result['PVALUE'] = 1-chi2.cdf(burden_Z**2,1)
-
-            result.to_csv(args.out_dir+'/CHR'+str(args.chr)+'_sumstat_assoc.txt',
-                          sep='\t',index=None,header=None,mode='a')
+    burden_Z=None
 
 ###############################################################
 ### thread process
