dec71cfa27fe9a0fe0efca98a9a8a8d4e33840eb
Artur <arturtjaro@gmail.com>
2023-04-10 10:18:10 -0700

    trying context manager for pool

--- TWAS/Asso_Study_02.py
+++ TWAS/Asso_Study_02.py
@@ -20,43 +20,43 @@ start_time = time()
 
 ###############################################################
 # parse input arguments
-parser = argparse.ArgumentParser(description='Asso Study 02')
+parser = argparse.ArgumentParser(description="Asso Study 02")
 
 # Specify tool directory
-parser.add_argument('--TIGAR_dir', type=str)
+parser.add_argument("--TIGAR_dir", type=str)
 
 # Gene annotation file path
-parser.add_argument('--gene_anno', type=str, dest='annot_path')
+parser.add_argument("--gene_anno", type=str, dest="annot_path")
 
 # chromosome number
-parser.add_argument('--chr', type=str, dest='chrm')
+parser.add_argument("--chr", type=str, dest="chrm")
 
 # Weight file path
-parser.add_argument('--weight', type=str, dest='w_path')
+parser.add_argument("--weight", type=str, dest="w_path")
 
 # GWAS Z score file path
-parser.add_argument('--Zscore', type=str, dest='z_path')
+parser.add_argument("--Zscore", type=str, dest="z_path")
 
 # Reference covariance file path
-parser.add_argument('--LD', type=str, dest='ld_path')
+parser.add_argument("--LD", type=str, dest="ld_path")
 
 # window
-parser.add_argument('--window',type=float)
+parser.add_argument("--window", type=float)
 
 # Weight threshold to include SNP in TWAS
-parser.add_argument('--weight_threshold',type=float)
+parser.add_argument("--weight_threshold", type=float)
 
 # specify 'FUSION', 'SPrediXcan', or 'both': Zscore test statistic to use
-parser.add_argument('--test_stat', type=str)
+parser.add_argument("--test_stat", type=str)
 
 # Number of threads
-parser.add_argument('--thread',type=int)
+parser.add_argument("--thread", type=int)
 
 # Output dir
-parser.add_argument('--out_dir', type=str)
+parser.add_argument("--out_dir", type=str)
 
 # output file
-parser.add_argument('--out_twas_file', type=str)
+parser.add_argument("--out_twas_file", type=str)
 
 args = parser.parse_args()
 
@@ -66,42 +66,50 @@ sys.path.append(args.TIGAR_dir)
 ## Import TIGAR functions, define other functions
 import TIGARutils as tg
 
-def get_pval(z): return np.format_float_scientific(chi2.sf(z**2, 1), precision=15, exp_digits=0)
+
+def get_pval(z):
+    return np.format_float_scientific(chi2.sf(z**2, 1), precision=15, exp_digits=0)
+
 
 def get_V_cor(V_cov):
-	V_cov = V_cov.copy()
-	v = np.sqrt(np.diag(V_cov))
-	outer_v = np.outer(v, v)
-	V_cor = V_cov / outer_v
-	V_cor[V_cov == 0] = 0
-	return V_cor
+    V_cov = V_cov.copy()
+    v = np.sqrt(np.diag(V_cov))
+    outer_v = np.outer(v, v)
+    V_cor = V_cov / outer_v
+    V_cor[V_cov == 0] = 0
+    return V_cor
+
 
 def get_z_denom(V, w):
-	return np.sqrt(np.linalg.multi_dot([w, V, w]))
+    return np.sqrt(np.linalg.multi_dot([w, V, w]))
+
 
 def get_spred_zscore(V_cov, w, Z_gwas, snp_sd):
-	Z_twas = snp_sd.dot(w * Z_gwas) / get_z_denom(V_cov, w)
-	return Z_twas, get_pval(Z_twas)
-	
+    Z_twas = snp_sd.dot(w * Z_gwas) / get_z_denom(V_cov, w)
+    return Z_twas, get_pval(Z_twas)
+
+
 def get_fusion_zscore(V_cov, w, Z_gwas, snp_sd=None):
-	V_cor = get_V_cor(V_cov)
-	Z_twas = np.vdot(Z_gwas, w) / get_z_denom(V_cor, w)
-	return Z_twas, get_pval(Z_twas)
+    V_cor = get_V_cor(V_cov)
+    Z_twas = np.vdot(Z_gwas, w) / get_z_denom(V_cor, w)
+    return Z_twas, get_pval(Z_twas)
+
 
 def get_burden_zscore(test_stat, get_zscore_args):
-	if test_stat =='FUSION':
-		return get_fusion_zscore(*get_zscore_args)
-	if test_stat == 'SPrediXcan':
-		return get_spred_zscore(*get_zscore_args)
+    if test_stat == "FUSION":
+        return get_fusion_zscore(*get_zscore_args)
+    if test_stat == "SPrediXcan":
+        return get_spred_zscore(*get_zscore_args)
+
 
 #############################################################
 # Print input arguments to log
 # out_twas_path = args.out_dir + '/CHR' + args.chrm + '_sumstat_assoc.txt'
 
-out_twas_path = args.out_dir + '/' + args.out_twas_file
+out_twas_path = args.out_dir + "/" + args.out_twas_file
 
 print(
-'''********************************
+    """********************************
 Input Arguments
 Gene annotation file specifying genes for TWAS: {annot_path}
 Chromosome: {chrm}
@@ -114,166 +122,173 @@ Test statistic to use: {test_stat_str}
 Number of threads: {thread}
 Output directory: {out_dir}
 Output TWAS results file: {out_path}
-********************************'''.format(
-	**args.__dict__,
-	test_stat_str = 'FUSION and SPrediXcan' if args.test_stat=='both' else args.test_stat,
-	out_path = out_twas_path))
+********************************""".format(
+        **args.__dict__,
+        test_stat_str="FUSION and SPrediXcan"
+        if args.test_stat == "both"
+        else args.test_stat,
+        out_path=out_twas_path
+    )
+)
 
 tg.print_args(args)
 
 ###############################################################
-### Read in gene annotation 
-print('Reading gene annotation file.')
+### Read in gene annotation
+print("Reading gene annotation file.")
 # Gene, TargetID, n_targets = tg.read_gene_annot_exp(args.annot_path, args.chrm)
 Gene, TargetID, n_targets = tg.read_gene_annot_exp(**args.__dict__)
 
 # read in headers for Weight and Zscore files; get the indices and dtypes for reading files into pandas
-print('Reading file headers.\n')
+print("Reading file headers.\n")
 weight_info = tg.weight_file_info(**args.__dict__)
 zscore_info = tg.zscore_file_info(**args.__dict__)
 
 # PREP OUTPUT - print output headers to files
-print('Creating file: ' + out_twas_path + '\n')
-out_cols = ['CHROM','GeneStart','GeneEnd','TargetID','GeneName','n_snps']
-if args.test_stat == 'both':
-	out_cols += ['FUSION_Z','FUSION_PVAL','SPred_Z','SPred_PVAL']
+print("Creating file: " + out_twas_path + "\n")
+out_cols = ["CHROM", "GeneStart", "GeneEnd", "TargetID", "GeneName", "n_snps"]
+if args.test_stat == "both":
+    out_cols += ["FUSION_Z", "FUSION_PVAL", "SPred_Z", "SPred_PVAL"]
 else:
-	out_cols += ['Zscore','PVALUE']
+    out_cols += ["Zscore", "PVALUE"]
 
 pd.DataFrame(columns=out_cols).to_csv(
-	out_twas_path,
-	sep='\t',
-	index=None,
-	header=True,
-	mode='w')
+    out_twas_path, sep="\t", index=None, header=True, mode="w"
+)
 
 ###############################################################
 # thread function
 @tg.error_handler
 def thread_process(num):
-	target = TargetID[num]
-	print('num=' + str(num) + '\nTargetID=' + target)
-	Gene_Info = Gene.iloc[[num]].reset_index(drop=True)
+    target = TargetID[num]
+    print("num=" + str(num) + "\nTargetID=" + target)
+    Gene_Info = Gene.iloc[[num]].reset_index(drop=True)
 
-	# get start and end positions to tabix
-	start = str(max(int(Gene_Info.GeneStart)-args.window,0))
-	end = str(int(Gene_Info.GeneEnd)+args.window)
+    # get start and end positions to tabix
+    start = str(max(int(Gene_Info.GeneStart) - args.window, 0))
+    end = str(int(Gene_Info.GeneEnd) + args.window)
 
-	# check that both files have data for target
-	tabix_query = tg.tabix_query_files(start, end, **args.__dict__)
+    # check that both files have data for target
+    tabix_query = tg.tabix_query_files(start, end, **args.__dict__)
 
-	if not tabix_query:
-		print('No test SNPs with non-zero cis-eQTL weights and/or no test SNPs GWAS Zscore for TargetID: ' + target + '.\n')
-		return None
+    if not tabix_query:
+        print(
+            "No test SNPs with non-zero cis-eQTL weights and/or no test SNPs GWAS Zscore for TargetID: "
+            + target
+            + ".\n"
+        )
+        return None
 
-	# read in weight data for target, filtered by weight_threshold
-	Weight = tg.read_tabix(start, end, target=target, **weight_info)
+    # read in weight data for target, filtered by weight_threshold
+    Weight = tg.read_tabix(start, end, target=target, **weight_info)
 
-	# read in Zscore data
-	Zscore = tg.read_tabix(start, end, **zscore_info)
+    # read in Zscore data
+    Zscore = tg.read_tabix(start, end, **zscore_info)
 
-	# get flipped snpIDs
-	Zscore['snpIDflip'] = tg.get_snpIDs(Zscore, flip=True)
+    # get flipped snpIDs
+    Zscore["snpIDflip"] = tg.get_snpIDs(Zscore, flip=True)
 
-	snp_overlap = np.intersect1d(Weight.snpID, Zscore[['snpID','snpIDflip']])
+    snp_overlap = np.intersect1d(Weight.snpID, Zscore[["snpID", "snpIDflip"]])
 
-	if not snp_overlap.size:
-		print('No overlapping test SNPs that have magnitude of cis-eQTL weights greater than threshold value and with GWAS Zscore for TargetID: ' + target + '.\n')
-		return None
+    if not snp_overlap.size:
+        print(
+            "No overlapping test SNPs that have magnitude of cis-eQTL weights greater than threshold value and with GWAS Zscore for TargetID: "
+            + target
+            + ".\n"
+        )
+        return None
 
-	# filter out non-matching snpID rows
-	Weight = Weight[Weight.snpID.isin(snp_overlap)]
-	Zscore = Zscore[np.any(Zscore[['snpID','snpIDflip']].isin(snp_overlap), axis=1)].reset_index(drop=True)	
+    # filter out non-matching snpID rows
+    Weight = Weight[Weight.snpID.isin(snp_overlap)]
+    Zscore = Zscore[
+        np.any(Zscore[["snpID", "snpIDflip"]].isin(snp_overlap), axis=1)
+    ].reset_index(drop=True)
 
-	# if not in Weight.snpIDs, assumed flipped; if flipped, flip Zscore sign
-	flip = np.where(Zscore.snpID.isin(Weight.snpID.values), 1, -1)
+    # if not in Weight.snpIDs, assumed flipped; if flipped, flip Zscore sign
+    flip = np.where(Zscore.snpID.isin(Weight.snpID.values), 1, -1)
 
-	if not np.all(flip == 1):
-		Zscore['snpID'] = np.where(flip == 1, Zscore.snpID, Zscore.snpIDflip)
-		Zscore['Zscore'] = flip * Zscore['Zscore']
+    if not np.all(flip == 1):
+        Zscore["snpID"] = np.where(flip == 1, Zscore.snpID, Zscore.snpIDflip)
+        Zscore["Zscore"] = flip * Zscore["Zscore"]
 
-	# drop unneeded columns
-	Zscore = Zscore.drop(columns=['CHROM','POS','REF','ALT','snpIDflip'])
+    # drop unneeded columns
+    Zscore = Zscore.drop(columns=["CHROM", "POS", "REF", "ALT", "snpIDflip"])
 
-	# # filter remaining by snpIDs
-	# snp_overlap = np.intersect1d(Weight.snpID, Zscore.snpID)
+    # # filter remaining by snpIDs
+    # snp_overlap = np.intersect1d(Weight.snpID, Zscore.snpID)
 
-	# if not snp_overlap.size:
-	# 	print('No overlapping test SNPs that have magnitude of cis-eQTL weights greater than threshold value and with GWAS Zscore for TargetID: ' + target + '\n')
-	# 	return None
+    # if not snp_overlap.size:
+    # 	print('No overlapping test SNPs that have magnitude of cis-eQTL weights greater than threshold value and with GWAS Zscore for TargetID: ' + target + '\n')
+    # 	return None
 
-	# Weight = Weight[Weight.snpID.isin(snp_overlap)]
-	# Zscore = Zscore[Zscore.snpID.isin(snp_overlap)]
+    # Weight = Weight[Weight.snpID.isin(snp_overlap)]
+    # Zscore = Zscore[Zscore.snpID.isin(snp_overlap)]
 
-	# merge Zscore and Weight dataframes on snpIDs
-	ZW = Weight.merge(Zscore[['snpID','Zscore']], 
-		left_on='snpID', 
-		right_on='snpID', 
-		how='inner')
+    # merge Zscore and Weight dataframes on snpIDs
+    ZW = Weight.merge(
+        Zscore[["snpID", "Zscore"]], left_on="snpID", right_on="snpID", how="inner"
+    )
 
-	snp_search_ids = ZW.snpID.values
+    snp_search_ids = ZW.snpID.values
 
-	# Read in reference covariance matrix file by snpID
-	MCOV = tg.get_ld_data(args.ld_path, snp_search_ids)
+    # Read in reference covariance matrix file by snpID
+    MCOV = tg.get_ld_data(args.ld_path, snp_search_ids)
 
-	if MCOV.empty:
-	  print('No reference covariance information for target SNPs for TargetID: ' + target + '\n')
-	  return None
+    if MCOV.empty:
+        print(
+            "No reference covariance information for target SNPs for TargetID: "
+            + target
+            + "\n"
+        )
+        return None
 
-	# get the snp variance and covariance matrix
-	snp_sd, V_cov = tg.get_ld_matrix(MCOV)
+    # get the snp variance and covariance matrix
+    snp_sd, V_cov = tg.get_ld_matrix(MCOV)
 
-	ZW = ZW[ZW.snpID.isin(MCOV.snpID)]
-	ZW = ZW.drop_duplicates(['snpID'], keep='first').reset_index(drop=True)
-	n_snps = str(ZW.snpID.size)
+    ZW = ZW[ZW.snpID.isin(MCOV.snpID)]
+    ZW = ZW.drop_duplicates(["snpID"], keep="first").reset_index(drop=True)
+    n_snps = str(ZW.snpID.size)
 
-	print('Running TWAS.\nN SNPs=' + n_snps)
+    print("Running TWAS.\nN SNPs=" + n_snps)
 
-	### create output dataframe
-	Result = Gene_Info.copy()
-	Result['n_snps'] = n_snps
+    ### create output dataframe
+    Result = Gene_Info.copy()
+    Result["n_snps"] = n_snps
 
-	### calculate zscore(s), pvalue(s)
-	# get_zscore_args = [V_cov, ZW, snp_sd]
-	get_zscore_args = [V_cov, ZW.ES.values, ZW.Zscore.values, snp_sd]
+    ### calculate zscore(s), pvalue(s)
+    # get_zscore_args = [V_cov, ZW, snp_sd]
+    get_zscore_args = [V_cov, ZW.ES.values, ZW.Zscore.values, snp_sd]
 
-	if args.test_stat == 'both':
-		Result['FUSION_Z'], Result['FUSION_PVAL'] = get_fusion_zscore(*get_zscore_args)
+    if args.test_stat == "both":
+        Result["FUSION_Z"], Result["FUSION_PVAL"] = get_fusion_zscore(*get_zscore_args)
 
-		Result['SPred_Z'], Result['SPred_PVAL'] = get_spred_zscore(*get_zscore_args)
+        Result["SPred_Z"], Result["SPred_PVAL"] = get_spred_zscore(*get_zscore_args)
 
-	else:
-		Result['TWAS_Zscore'], Result['PVALUE'] = get_burden_zscore(args.test_stat, get_zscore_args)
+    else:
+        Result["TWAS_Zscore"], Result["PVALUE"] = get_burden_zscore(
+            args.test_stat, get_zscore_args
+        )
 
-	# write to file
-	Result.to_csv(
-		out_twas_path,
-		sep='\t',
-		index=None,
-		header=None,
-		mode='a')
+    # write to file
+    Result.to_csv(out_twas_path, sep="\t", index=None, header=None, mode="a")
 
-	print('Target TWAS completed.\n')
+    print("Target TWAS completed.\n")
+
 
 ###############################################################
 # thread process
-if __name__ == '__main__':
-	print('Starting TWAS for ' + str(n_targets) + ' target genes.\n')
-	pool = multiprocessing.Pool(args.thread)
-	pool.imap(thread_process,[num for num in range(n_targets)])
-	pool.close()
-	pool.join()
-	print('Done.')
+if __name__ == "__main__":
+    print("Starting TWAS for " + str(n_targets) + " target genes.\n")
+    with multiprocessing.Pool(args.thread) as pool:
+        pool.imap(thread_process, range(n_targets))
+    # pool = multiprocessing.Pool(args.thread)
+    # pool.imap(thread_process,[num for num in range(n_targets)])
+    # pool.close()
+    # pool.join()
+    print("Done.")
 
 ###############################################################
 # time calculation
-elapsed_sec = time()-start_time
+elapsed_sec = time() - start_time
 elapsed_time = tg.format_elapsed_time(elapsed_sec)
-print('Computation time (DD:HH:MM:SS): ' + elapsed_time)
-
-
-
-
-
-
-
+print("Computation time (DD:HH:MM:SS): " + elapsed_time)
--- TWAS/Get_LD.py
+++ TWAS/Get_LD.py
@@ -20,40 +20,40 @@ start_time = time()
 
 ###############################################################
 # parse input arguments
-parser = argparse.ArgumentParser(description='Get LD')
+parser = argparse.ArgumentParser(description="Get LD")
 
 # Specify tool directory
-parser.add_argument('--TIGAR_dir', type=str)
+parser.add_argument("--TIGAR_dir", type=str)
 
 # chromosome block information path
-parser.add_argument('--genome_block', type=str, dest='block_path')
+parser.add_argument("--genome_block", type=str, dest="block_path")
 
 # sampleID path
-parser.add_argument('--sampleID', type=str, dest='sampleid_path')
+parser.add_argument("--sampleID", type=str, dest="sampleid_path")
 
 # chromosome number
-parser.add_argument('--chr', type=str, dest='chrm')
+parser.add_argument("--chr", type=str, dest="chrm")
 
 # genotype file path
-parser.add_argument('--genofile', type=str, dest='geno_path')
+parser.add_argument("--genofile", type=str, dest="geno_path")
 
 # specified input file type (vcf or doasges)
-parser.add_argument('--genofile_type', type=str)
+parser.add_argument("--genofile_type", type=str)
 
 # 'DS' or 'GT'
-parser.add_argument('--format', type=str, dest='data_format')
+parser.add_argument("--format", type=str, dest="data_format")
 
 # maf threshold for seleting genotype data to calculate covariance matrix
-parser.add_argument('--maf', type=float)
+parser.add_argument("--maf", type=float)
 
 # number of threads
-parser.add_argument('--thread', type=int)
+parser.add_argument("--thread", type=int)
 
 # output file path
-parser.add_argument('--out_ld_file', type=str)
+parser.add_argument("--out_ld_file", type=str)
 
 # output dir
-parser.add_argument('--out_dir', type=str)
+parser.add_argument("--out_dir", type=str)
 
 args = parser.parse_args()
 
@@ -63,41 +63,52 @@ sys.path.append(args.TIGAR_dir)
 # DEFINE, IMPORT FUNCTIONS
 import TIGARutils as tg
 
+
 def block_error(e):
-	print('Fatal error in block LD calculation.\n')
-	pool.terminate()
+    raise Exception("Fatal error in block LD calculation.\n")
+    # pool.terminate()
+
 
 # limit to 4 decimal places max, strip trailing 0s
-def cov_fmt(x): return ('%.4f' % x).rstrip('0').rstrip('.')
+def cov_fmt(x):
+    return ("%.4f" % x).rstrip("0").rstrip(".")
+
 
 # trim array by positionin matrix (length should be rownumber:total for each row);
 # format each element in each row, join all together separated by comma
-def cov_str(cov_lst): return [','.join([cov_fmt(x) for x in row]) for row in [cov_lst[i][i:len(cov_lst)] for i in range(len(cov_lst))]]
+def cov_str(cov_lst):
+    return [
+        ",".join([cov_fmt(x) for x in row])
+        for row in [cov_lst[i][i : len(cov_lst)] for i in range(len(cov_lst))]
+    ]
+
 
 def out_block_path(num):
-	return(args.out_dir + '/' + args.out_ld_file + '_block_' + str(num) + '.txt')
+    return args.out_dir + "/" + args.out_ld_file + "_block_" + str(num) + ".txt"
 
-# def out_block_path(num):
-# 	return(args.out_dir + '/' + args.out_ld_file + '_block_' + str(num) + '.txt.gz')
 
 ###############################################################
 # check input arguments
-if args.genofile_type == 'vcf':
-	if (args.data_format != 'GT') and (args.data_format != 'DS'):
-		raise SystemExit('Please specify the genotype data format used by the vcf file (--format ) as either "GT" or "DS".\n')
-		
-elif args.genofile_type == 'dosage':
-	args.data_format = 'DS'
+if args.genofile_type == "vcf":
+    if (args.data_format != "GT") and (args.data_format != "DS"):
+        raise SystemExit(
+            'Please specify the genotype data format used by the vcf file (--format ) as either "GT" or "DS".\n'
+        )
+
+elif args.genofile_type == "dosage":
+    args.data_format = "DS"
 
 else:
-	raise SystemExit('Please specify the type input genotype file type (--genofile_type) as either "vcf" or "dosage".\n')
+    raise SystemExit(
+        'Please specify the type input genotype file type (--genofile_type) as either "vcf" or "dosage".\n'
+    )
 
-out_refcovld_path = args.out_dir + '/' + args.out_ld_file + '.txt'
+out_refcovld_path = args.out_dir + "/" + args.out_ld_file + ".txt"
 
 ###############################################################
 # Print input arguments
 print(
-'''********************************
+    """********************************
 Input Arguments
 Genome block annotation (based on LD structure) file: {block_path}
 SampleID file: {sampleid_path}
@@ -109,23 +120,25 @@ MAF threshold for SNP inclusion: {maf}
 Number of threads: {thread}
 Output directory: {out_dir}
 Output reference covariance results file: {out_rc}
-********************************'''.format(
-	**args.__dict__,
-	out_rc = out_refcovld_path))
+********************************""".format(
+        **args.__dict__, out_rc=out_refcovld_path
+    )
+)
 
 # tg.print_args(args)
 
 ###############################################################
 # Read in block information
-print('Reading block annotation file.')
+print("Reading block annotation file.")
 
 # read in block file
 Blocks = pd.read_csv(
-	args.block_path,
-	sep='\t',
-	usecols=['CHROM','Start','End'],
-	dtype={'CHROM':object, 'Start':object, 'End':object})
-Blocks = Blocks[Blocks['CHROM'] == args.chrm].reset_index(drop=True)
+    args.block_path,
+    sep="\t",
+    usecols=["CHROM", "Start", "End"],
+    dtype={"CHROM": object, "Start": object, "End": object},
+)
+Blocks = Blocks[Blocks["CHROM"] == args.chrm].reset_index(drop=True)
 Blocks = tg.optimize_cols(Blocks)
 n_blocks = len(Blocks)
 
@@ -133,63 +146,64 @@ n_blocks = len(Blocks)
 sampleID, sample_size, geno_info = tg.sampleid_startup(**args.__dict__)
 
 # write columns out to file
-print('Creating file: ' + out_refcovld_path + '\n')
-out_cols = ['#0','snpID','CHROM','POS','COV']
+print("Creating file: " + out_refcovld_path + "\n")
+out_cols = ["#0", "snpID", "CHROM", "POS", "COV"]
 pd.DataFrame(columns=out_cols).to_csv(
-	out_refcovld_path,
-	sep='\t',
-	index=None,
-	header=True,
-	mode='w')
+    out_refcovld_path, sep="\t", index=None, header=True, mode="w"
+)
 
-print('********************************\n')
+print("********************************\n")
 
 ###############################################################
 @tg.fatal_error_handler
 def thread_process(num):
-	Block = Blocks.loc[num]
-	print('num=' + str(num))
-	
-	# read in and process genotype data; file must be bgzipped/tabix
-	Geno = tg.read_tabix(Block.Start, Block.End, sampleID, **geno_info)
+    Block = Blocks.loc[num]
+    print("num=" + str(num))
 
-	# calculate, filter maf
-	Geno = tg.calc_maf(Geno, sampleID, args.maf, filter_bid=True)
+    # read in and process genotype data; file must be bgzipped/tabix
+    Geno = tg.read_tabix(Block.Start, Block.End, sampleID, **geno_info)
 
-	# get upper covariance matrix
-	mcovar = np.triu(np.cov(Geno[sampleID].values)).tolist()
+    # calculate, filter maf
+    Geno = tg.calc_maf(Geno, sampleID, args.maf, filter_bid=True)
 
-	# output values
-	Geno = Geno[['snpID', 'CHROM', 'POS']]
-	Geno['COV'] = cov_str(mcovar)
-	Geno.to_csv(
-		out_block_path(num),
-		sep='\t',
-		index=None,
-		header=None,
-		# compression='gzip',
-		mode='a')
+    # get upper covariance matrix
+    mcovar = np.triu(np.cov(Geno[sampleID].values)).tolist()
 
-	print('Block LD calculation completed for block.\n')
+    # output values
+    Geno = Geno[["snpID", "CHROM", "POS"]]
+    Geno["COV"] = cov_str(mcovar)
+    Geno.to_csv(
+        out_block_path(num),
+        sep="\t",
+        index=None,
+        header=None,
+        # compression='gzip',
+        mode="a",
+    )
+
+    print("Block LD calculation completed for block.\n")
 
 
 ##################################################################
 # thread process
-if __name__ == '__main__':
-	print('Starting LD calculation for ' + str(n_blocks) + ' blocks.\n')
-	# global pool
-	# pool = multiprocessing.Pool(args.thread)
-	# # pool.imap(thread_process,[num for num in range(n_blocks)])
-	# pool.map_async(thread_process,[num for num in range(n_blocks)], error_callback=block_error)
-	# pool.close()
-	# pool.join()
-	# Parallel(n_jobs=args.thread)(delayed(thread_process)(num) for num in range(n_blocks))
-	for num in range(n_blocks):
-		thread_process(num)
-	print('Done.')
+if __name__ == "__main__":
+    print("Starting LD calculation for " + str(n_blocks) + " blocks.\n")
+    with multiprocessing.Pool(args.thread) as pool:
+        pool.map_async(thread_process, range(n_blocks), error_callback=block_error)
+
+    # global pool
+    # pool = multiprocessing.Pool(args.thread)
+    # # pool.imap(thread_process,[num for num in range(n_blocks)])
+    # pool.map_async(thread_process,[num for num in range(n_blocks)], error_callback=block_error)
+    # pool.close()
+    # pool.join()
+    # Parallel(n_jobs=args.thread)(delayed(thread_process)(num) for num in range(n_blocks))
+    # for num in range(n_blocks):
+    # 	thread_process(num)
+    print("Done.")
 
 ################################################################
 # time calculation
-elapsed_sec = time()-start_time
+elapsed_sec = time() - start_time
 elapsed_time = tg.format_elapsed_time(elapsed_sec)
-print('Computation time (DD:HH:MM:SS): ' + elapsed_time)
+print("Computation time (DD:HH:MM:SS): " + elapsed_time)
