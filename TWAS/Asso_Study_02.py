50a1f38b4d0cf17f4646a94e1f7f12a3941bedfd
Artur <arturtjaro@gmail.com>
2023-04-12 16:48:34 -0700

    revert.. writing doesn't work

--- TWAS/Asso_Study_02.py
+++ TWAS/Asso_Study_02.py
@@ -14,7 +14,6 @@ import pandas as pd
 
 from scipy.stats import chi2
 from joblib import Parallel, delayed
-from google.cloud import storage
 import threading
 
 
@@ -75,20 +74,12 @@ def main():
     parser.add_argument("--out_twas_file", type=str)
 
     args = parser.parse_args()
-    n = np.random.randint(100)
-
-    def write_to_file(stuff, pid):
-        storage_client = storage.Client("rome-repeat-genetics")
-        bucket = storage_client.bucket("rome-repeat-genetics")
-        blob = bucket.blob(f"output_logs.{args.chrm}.{n}.{pid}.txt")
-        with blob.open("w") as f:
-            f.write(f"{stuff}\n")
 
     sys.path.append(args.TIGAR_dir)
     import TIGARutils as tg
 
     out_twas_path = args.out_dir + "/" + args.out_twas_file
-    write_to_file(
+    print(
         """********************************
         Input Arguments
         Gene annotation file specifying genes for TWAS: {annot_path}
@@ -108,19 +99,18 @@ def main():
             if args.test_stat == "both"
             else args.test_stat,
             out_path=out_twas_path
-        ),
-        "main",
+        )
     )
 
     tg.print_args(args)
 
 
-    write_to_file("Reading gene annotation file.", "main")
+    print("Reading gene annotation file.")
     # Gene, TargetID, n_targets = tg.read_gene_annot_exp(args.annot_path, args.chrm)
     Gene, TargetID, n_targets = tg.read_gene_annot_exp(**args.__dict__)
 
     # read in headers for Weight and Zscore files; get the indices and dtypes for reading files into pandas
-    write_to_file("Reading file headers.\n", "main")
+    print("Reading file headers.\n")
     weight_info = tg.weight_file_info(**args.__dict__)
     zscore_info = tg.zscore_file_info(**args.__dict__)
 
@@ -135,7 +125,7 @@ def main():
     @tg.error_handler
     def thread_process(num):
         target = TargetID[num]
-        write_to_file("num=" + str(num) + "\nTargetID=" + target, num)
+        print("num=" + str(num) + "\nTargetID=" + target)
         Gene_Info = Gene.iloc[[num]].reset_index(drop=True)
 
         # get start and end positions to tabix
@@ -143,23 +133,20 @@ def main():
         end = str(int(Gene_Info.GeneEnd) + args.window)
 
         # check that both files have data for target
-        write_to_file("reading query", num)
         tabix_query = tg.tabix_query_files(start, end, **args.__dict__)
 
         if not tabix_query:
-            write_to_file(
+            print(
                 "No test SNPs with non-zero cis-eQTL weights and/or no test SNPs GWAS Zscore for TargetID: "
                 + target
-                + ".", num
+                + "."
             )
             return None
 
         # read in weight data for target, filtered by weight_threshold
-        write_to_file("reading Weight", num)
         Weight = tg.read_tabix(start, end, target=target, semaphore_key="w", **weight_info)
 
         # read in Zscore data
-        write_to_file("reading Zscore", num)
         Zscore = tg.read_tabix(start, end, semaphore_key="z", **zscore_info)
 
         # get flipped snpIDs
@@ -168,15 +155,14 @@ def main():
         snp_overlap = np.intersect1d(Weight.snpID, Zscore[["snpID", "snpIDflip"]])
 
         if not snp_overlap.size:
-            write_to_file(
+            print(
                 "No overlapping test SNPs that have magnitude of cis-eQTL weights greater than threshold value and with GWAS Zscore for TargetID: "
                 + target
-                + ".\n", num
+                + ".\n"
             )
             return None
 
         # filter out non-matching snpID rows
-        write_to_file("step 1", num)
         Weight = Weight[Weight.snpID.isin(snp_overlap)]
         Zscore = Zscore[
             np.any(Zscore[["snpID", "snpIDflip"]].isin(snp_overlap), axis=1)
@@ -185,7 +171,6 @@ def main():
         # if not in Weight.snpIDs, assumed flipped; if flipped, flip Zscore sign
         flip = np.where(Zscore.snpID.isin(Weight.snpID.values), 1, -1)
 
-        write_to_file("step 2", num)
         if not np.all(flip == 1):
             Zscore["snpID"] = np.where(flip == 1, Zscore.snpID, Zscore.snpIDflip)
             Zscore["Zscore"] = flip * Zscore["Zscore"]
@@ -194,7 +179,6 @@ def main():
         Zscore = Zscore.drop(columns=["CHROM", "POS", "REF", "ALT", "snpIDflip"])
 
         # merge Zscore and Weight dataframes on snpIDs
-        write_to_file("step 3", num)
         ZW = Weight.merge(
             Zscore[["snpID", "Zscore"]], left_on="snpID", right_on="snpID", how="inner"
         )
@@ -202,39 +186,35 @@ def main():
         snp_search_ids = ZW.snpID.values
 
         # Read in reference covariance matrix file by snpID
-        write_to_file("step 4", num)
         MCOV = tg.get_ld_data(args.ld_path, snp_search_ids)
 
         if MCOV.empty:
-            write_to_file(
+            print(
                 "No reference covariance information for target SNPs for TargetID: "
                 + target
-                + "\n", num
+                + "\n"
             )
             return None
 
         # get the snp variance and covariance matrix
-        write_to_file("step 5", num)
         snp_sd, V_cov = tg.get_ld_matrix(MCOV)
 
         ZW = ZW[ZW.snpID.isin(MCOV.snpID)]
         ZW = ZW.drop_duplicates(["snpID"], keep="first").reset_index(drop=True)
         n_snps = str(ZW.snpID.size)
 
-        write_to_file("Running TWAS.\nN SNPs=" + n_snps, num)
+        print("Running TWAS.\nN SNPs=" + n_snps)
 
         ### create output dataframe
         Result = Gene_Info.copy()
         Result["n_snps"] = n_snps
 
         ### calculate zscore(s), pvalue(s)
-        write_to_file("step 6", num)
         get_zscore_args = [V_cov, ZW.ES.values, ZW.Zscore.values, snp_sd]
 
         if args.test_stat == "both":
-            write_to_file("step 7", num)
             Result["FUSION_Z"], Result["FUSION_PVAL"] = get_fusion_zscore(*get_zscore_args)
-            write_to_file("step 8", num)
+
             Result["SPred_Z"], Result["SPred_PVAL"] = get_spred_zscore(*get_zscore_args)
 
         else:
@@ -245,10 +225,10 @@ def main():
         # write to file
         # Result.to_csv(out_twas_path, sep="\t", index=None, header=None, mode="a")
 
-        write_to_file("Target TWAS completed.\n", num)
+        print("Target TWAS completed.\n")
         return Result
 
-    write_to_file("Starting TWAS for " + str(n_targets) + " target genes.\n", "main")
+    print("Starting TWAS for " + str(n_targets) + " target genes.\n")
     res = Parallel(n_jobs=args.thread)(delayed(thread_process)(num) for num in range(n_targets))
 
     # with multiprocessing.Pool(args.thread) as pool:
@@ -265,14 +245,14 @@ def main():
     # pool.imap(thread_process,[num for num in range(n_targets)])
     # pool.close()
     # pool.join()
-    write_to_file("Done.", "main")
+    print("Done.")
 
 
     ###############################################################
     # time calculation
     elapsed_sec = time() - start_time
     elapsed_time = tg.format_elapsed_time(elapsed_sec)
-    write_to_file("Computation time (DD:HH:MM:SS): " + elapsed_time, "main")
+    print("Computation time (DD:HH:MM:SS): " + elapsed_time)
 
 
 ###############################################################
